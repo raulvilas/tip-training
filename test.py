fffffefc523a65d9c6b61736fb81e577b3e59c69
Raul Vilas <raul.vilaspineiro@tip-group.com>
2024-03-14 16:21:52 +0100

    fibonacci squence

--- test.py
+++ test.py
@@ -1,6 +1,28 @@
 import os
 import sys
 
-print('Hello guys this is a testuing')
-print('Hello this is the new print')
-print('This is the print that you are gonna need to add for the rebase')
+# Program to display the Fibonacci sequence up to n-th term
+
+nterms = int(input("How many terms? "))
+
+# first two terms
+n1, n2 = 0, 1
+count = 0
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+# if there is only one term, return n1
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+# generate fibonacci sequence
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
